b89f0e5d8cb405abf5c009c866a6ba38a1a77b96
bonzei <bonzei123@gmail.com>
2023-03-10 15:05:44 +0100

    first prototype

--- forms.py
+++ forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import (StringField, TextAreaField, IntegerField, BooleanField,
-                     RadioField, SelectField)
+from wtforms import (SelectField, HiddenField)
 from wtforms.validators import InputRequired, Length
 
 
 class ZensusForm(FlaskForm):
     schuld = SelectField('Wer ist Schuld?', choices=[('1', 'jobaxgaming'), ('2', 'jobaxgaming'), ('3', 'Philipp Lahm'), ('4', 'jobaxgaming'), ])
+    user = HiddenField('user')
+    state = HiddenField('state')

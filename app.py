da692d2ef1da0aa9dabef2c16aee8182e193c629
fw <floriwenzel@aol.com>
2023-03-11 17:06:45 +0100

    removing all cache stuff changing userid to userhash of name and id adding timestamp and cleanup for State

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ class Entry(db.Model):
 
 
 class State(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     state = db.Column(db.String(), nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow())
 

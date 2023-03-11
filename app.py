df554ee001f51dfaa9c876ba8b321da2ef100efe
fw <floriwenzel@aol.com>
2023-03-11 15:09:04 +0100

    get_userdata fix

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ def get_userdata(access_token):
     headers.update({"Authorization": "bearer " + access_token})
     response = requests.get("https://oauth.reddit.com/api/v1/me", headers=headers)
     me_json = response.json()
-    ret = {'name': me_json['name'], 'created': me_json['created'], 'id': me_json['userid']}
+    ret = {'name': me_json['name'], 'created': me_json['created'], 'userid': me_json['id']}
     return ret
 
 
